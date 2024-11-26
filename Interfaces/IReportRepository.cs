9271773d58b6eb97c2142c7e47984a689378f40f
zach colburn <colburn.zach7@gmail.com>
2024-11-26 17:28:59 -0600

    Get Single and all Reports, also add equipment

--- Interfaces/IReportRepository.cs
+++ Interfaces/IReportRepository.cs
@@ -5,6 +5,7 @@ namespace BECapstoneIronAssist.Interfaces
     public interface IReportRepository
     {
         Task <List<Report>> GetAllReportsAsync();
+        Task <Report> GetSingleReportAsync(int id);
         Task <Report> AddReportAsync(Report newReport);
         Task <Report> UpdateSingleReportAsync(int id, Report newReport);
         Task <Report> DeleteSingleReportAsync(int id);
